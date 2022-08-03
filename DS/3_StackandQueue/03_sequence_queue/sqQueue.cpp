66e85d2ae84bc52b60a41e86a2a42633fa6a6cd0
klaus <18848113328@163.com>
2022-08-03 10:23:37 +0800

    3.2.2 is done

---
+++ DS/3_StackandQueue/03_sequence_queue/sqQueue.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+/******************Declaration*******************/
+#define MAXSIZE 10
+typedef struct SqQueue
+{
+    int data[MAXSIZE];
+    int front, rear;
+} SqQueue;
+
+void InitQueue(SqQueue &Q)
+{
+    Q.rear = Q.front = 0;
+}
+
+bool Empty(SqQueue Q)
+{
+    return (Q.rear == Q.front);
+}
+/******************In and Out*******************/
+bool EnQueue(SqQueue &Q, int x)
+{
+    if ((Q.rear + 1) % MAXSIZE == Q.front)
+        return false;
+    Q.data[Q.rear] = x;
+    Q.rear = (Q.rear + 1) % MAXSIZE;
+    return true;
+}
+
+bool DeQueue(SqQueue &Q, int x)
+{
+    if (Q.front == Q.rear)
+        return false;
+    x = Q.data[Q.front];
+    Q.front = (Q.front + 1) % MAXSIZE;
+    return true;
+}
+
+/******************Read head*******************/
+bool GetHead(SqQueue Q, int x)
+{
+    if (Q.front == Q.rear)
+        return false;
+    x = Q.data[Q.front];
+    return true;
+}
+
+/******************Length*******************/
+int Length(SqQueue Q)
+{
+    return ((Q.rear - Q.front + MAXSIZE) % MAXSIZE);
+}
